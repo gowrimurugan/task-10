6fdaf3e8fa14d347ab86d95feb83c8272399df29
gowrimurugan <107912689+gowrimurugan@users.noreply.github.com>
2022-09-11 13:49:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let num1 = 10;
+
+if(num1 > 10){
+    console.log(num1 + " is greater than 10")
+}else if (num1 == 10){
+    console.log(num1 + " is equal to 10")
+}else {
+    console.log(num1 + " is less than 10")
+}
